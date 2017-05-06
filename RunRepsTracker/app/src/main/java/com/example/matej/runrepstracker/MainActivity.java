f7621f0977a8f3d371752db5830f50a54eddc8ef
matej94 <matej.tomic123gmail.com>
2017-05-06 13:58:22 +0200

    MainAcivity basic elements

---
+++ RunRepsTracker/app/src/main/java/com/example/matej/runrepstracker/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.matej.runrepstracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView AppTitleTv;
+    Button PushUpBtn, SitUpBtn, RunBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpUI();
+    }
+    private void setUpUI(){
+        AppTitleTv = (TextView) findViewById(R.id.AppTitleTv);
+        PushUpBtn = (Button) findViewById(R.id.PushUpBtn);
+        PushUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(),PushUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        SitUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(),SitUpActivity.class);
+                startActivity(intent);
+            }
+        });
+        RunBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(),RunActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ RunRepsTracker/app/src/main/java/com/example/matej/runrepstracker/SitUpActivity.java
@@ -0,0 +1,13 @@
+package com.example.matej.runrepstracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SitUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sit_up);
+    }
+}
